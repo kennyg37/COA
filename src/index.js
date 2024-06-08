109a039f60be7389b6dc0ee7763ba021790cbe4b
kennyg37 <k.ganza@alustudent.com>
2024-06-09 01:23:40 +0200

    adding basics features to the gallery

---
+++ src/index.js
@@ -0,0 +1,29 @@
+// a preloader to make the user experience better and smoother
+function showPreloader() {
+    document.getElementById('preloader').style.display = 'flex';
+  }
+  function hidePreloader() {
+    document.getElementById('preloader').style.display = 'none';
+  }
+
+// a function to show on the gallery by clicking a button but calling the showPreloader function first
+  document.getElementById('load-button').addEventListener('click', function() {
+    showPreloader();
+    setTimeout (function(){
+      showGallery();
+      hidePreloader();
+    }, 3000);
+  });
+
+  function showGallery() {
+    document.querySelector('.gallery').style.display = 'block';
+    document.querySelector('.heading').style.display = 'none';
+    document.querySelector('.close').style.display = 'block';
+
+  }
+
+  function hideGallery() {
+    document.querySelector('.gallery').style.display = 'none';
+    document.querySelector('.heading').style.display = 'block';
+    document.querySelector('.close').style.display = 'none';
+  }
